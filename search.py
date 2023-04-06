63a601f5bc66b593192e604b9df51806bccd968e
Sergio-prog <sergioreklamplay@gmail.com>
2023-04-07 00:30:57 +0300

    Added search by size method

--- search.py
+++ search.py
@@ -43,7 +43,7 @@ class Search:
         self.final = end - start
         return correct_paths
 
-    def search_by_name(self, name: str, path_object_returns: bool = False) -> List[str]:
+    def search_by_name(self, file_name: str, path_object_returns: bool = False) -> List[str]:
         """
         Returns all directories from the specified directory and specified suffix
 
@@ -52,14 +52,47 @@ class Search:
 
             print(search.search_by_name("osu!.exe", path_object_returns=False))
 
-            \>>> 'C:\\Games\\osu!\\osu!.exe'
+            \>>> ['C:\\Games\\osu!\\osu!.exe']
         """
         start = time()
 
         correct_paths = []
         for rootdir, dirs, files in os.walk(self.path):
             for file in files:
-                if Path(file).name == name:
+                if Path(file).name == file_name:
+                    goal_path = str(Path(rootdir, file)) if not path_object_returns else Path(rootdir, file)
+                    correct_paths.append(goal_path)
+
+        end = time()
+        self.final = end - start
+        return correct_paths
+
+    @property
+    def get_last_search_time(self):
+        return self.final
+
+    def search_by_size(self, file_size: Union[int, float], round_search_file_size: bool = True,
+                       path_object_returns: bool = False) -> List[str]:
+        """
+        Returns all directories from the specified directory and specified size in bytes
+
+        Example:
+            search = Search(r"C:/Games")
+
+            print(search.search_by_size(1024, round_search_file_size=True))
+
+            \>>> 'C:\\Games\\osu!\\osu!.exe'
+        """
+
+        start = time()
+
+        correct_paths = []
+        for rootdir, dirs, files in os.walk(self.path):
+            for file in files:
+                search_file = round(
+                    os.path.getsize(os.path.join(rootdir, file))) if round_search_file_size else os.path.getsize(
+                    os.path.join(rootdir, file))
+                if search_file == file_size:
                     goal_path = str(Path(rootdir, file)) if not path_object_returns else Path(rootdir, file)
                     correct_paths.append(goal_path)
 
@@ -73,5 +106,5 @@ if __name__ == "__main__":
 
     folder = r"D:/Games"  # or Path(r"D:/Games")
     search = Search(folder)
-    print(search.search_by_suffix(".wav"))
-    print(search.final)
+    print(search.search_by_size(10000, round_search_file_size=True, path_object_returns=True))
+    print(search.get_last_search_time)  # Returns the time it took to search
